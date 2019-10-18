8229352486802c5aeea575dddc7f57a475e6d733
Erin Polley <erinepolley@gmail.com>
2019-10-18 15:09:21 -0500

    added some comments to exercise

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,19 @@
+//grabbing header and inserting a title using the textContent property
 const artheader=document.querySelector(".article__header");
 artheader.textContent="Welcome to the Erin Polley blog.";
 
+//Changing the class of article header to important class.
 const headerImportant=document.querySelectorAll(".article__header");
 for(let i=0; i < headerImportant.length; i++) {
     headerImportant[i].classList.remove("article__header");
     headerImportant[i].classList.add("important");
 }
+//checking work
 console.log(headerImportant);
-
+//removing class dashed 
 const dashed = document.querySelector(".dashed");
 dashed.classList.remove("dashed");
 
 const golden = document.querySelector(".article__footer");
 golden.classList.add("goldenrod");
+
